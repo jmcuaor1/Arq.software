93f8f83060e74554c2017f224bf52c03cfeb5f66
Kevin Emmanuel Pabón Niño <143535629+kepabonsaeafit@users.noreply.github.com>
2026-05-20 01:27:34 -0500

    fix: actualizar IP a Elastic IP 44.207.202.98

--- frontend_react/src/services/api.js
+++ frontend_react/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://44.203.209.134:8080/api';
+const API_URL = 'http://44.207.202.98:8080/api';
 
 export const fetchProducts = async () => {
     const response = await fetch(`${API_URL}/productos/`);
